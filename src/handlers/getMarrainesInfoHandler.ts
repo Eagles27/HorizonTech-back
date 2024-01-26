0c753284390f5db122099f2b458a3c17c748b559
clemencebellet <clemence.bellet@edu.ece.fr>
2024-01-26 10:10:44 +0100

    feat(marraines): form responses

--- src/handlers/getMarrainesInfoHandler.ts
+++ src/handlers/getMarrainesInfoHandler.ts
@@ -1,7 +1,45 @@
+// import { FastifyReply, FastifyRequest } from 'fastify'
+// import requireAuth from '@/middleware/requireAuth'
+// import { USER_MONGOOSE } from '@/schema/mongoose'
+// import { TUser } from '@/schema/user'
+
+// type TRequest = FastifyRequest<{
+//   Headers: {
+//     authorization: string
+//   }
+// }>
+
+// const getMarraineHandler = async (req: TRequest, res: FastifyReply) => {
+//   const userId = await requireAuth(req, res)
+//   if (!userId) return
+
+//   try {
+//     const marraineResponse = await USER_MONGOOSE.find({ role: 'Marraine' }).select(
+//       '_id firstname lastname email finishedSignup role'
+//     )
+
+//     const marraine: TUser[] = marraineResponse.map((userResponse) => ({
+//       _id: userResponse._id.toString(),
+//       firstname: userResponse.firstname,
+//       lastname: userResponse.lastname,
+//       email: userResponse.email,
+//       finishedSignup: userResponse.finishedSignup,
+//       role: userResponse.role,
+//     }))
+
+//     res.send(marraine)
+//   } catch (error) {
+//     console.error(error)
+//     res.internalServerError()
+//   }
+// }
+
+// export default getMarraineHandler
 import { FastifyReply, FastifyRequest } from 'fastify'
 import requireAuth from '@/middleware/requireAuth'
-import { USER_MONGOOSE } from '@/schema/mongoose'
+import { USER_MONGOOSE, FORM_ANSWER_MONGOOSE } from '@/schema/mongoose'
 import { TUser } from '@/schema/user'
+import { TFormAnswers } from '@/schema/form_answer'
 
 type TRequest = FastifyRequest<{
   Headers: {
@@ -18,16 +56,17 @@ const getMarraineHandler = async (req: TRequest, res: FastifyReply) => {
       '_id firstname lastname email finishedSignup role'
     )
 
-    const marraine: TUser[] = marraineResponse.map((userResponse) => ({
-      _id: userResponse._id.toString(),
-      firstname: userResponse.firstname,
-      lastname: userResponse.lastname,
-      email: userResponse.email,
-      finishedSignup: userResponse.finishedSignup,
-      role: userResponse.role,
-    }))
+    const marraines: (TUser & { responses: TFormAnswers['responses'] })[] = await Promise.all(
+      marraineResponse.map(async (userResponse) => {
+        const formAnswers = await FORM_ANSWER_MONGOOSE.findOne({ user_id: userResponse._id })
+        return {
+          ...userResponse.toObject(),
+          responses: formAnswers ? formAnswers.responses : [],
+        }
+      })
+    )
 
-    res.send(marraine)
+    res.send(marraines)
   } catch (error) {
     console.error(error)
     res.internalServerError()
--- src/schema/info_marraine.ts
+++ src/schema/info_marraine.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod'
 
+const FORM_ANSWER_ELEMENT = z.object({
+  question: z.string(),
+  response: z.string(),
+})
 const MARRAINE_ELEMENT = z.object({
-  _id: z.string(),
   firstname: z.string(),
   lastname: z.string(),
   email: z.string(),
   finishedSignup: z.boolean(),
   role: z.enum(['Etudiante', 'Marraine']),
+  responses: z.array(FORM_ANSWER_ELEMENT),
 })
 const MARRAINE_INFO = z.array(MARRAINE_ELEMENT)
 
